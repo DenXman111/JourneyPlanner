e74f22ab99acdc0dba58ef0eb8c77080ae292b9d
Denis Pivovarov <denxman123@gmail.com>
2019-04-17 19:48:07 +0200

    Change scene after clicking to start

--- src/Controllers/WelcomeController.java
+++ src/Controllers/WelcomeController.java
@@ -2,15 +2,34 @@ package Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class WelcomeController {
 
+    Stage prevStage;
     @FXML
     private Button WelcomeSceneStartButton;
 
-    @FXML
-    void startButtonPressed(ActionEvent event) {
-        System.out.println("I should change scene : )");
+    public void setPrevStage(Stage stage){
+        this.prevStage = stage;
+    }
+
+    public void startButtonPressed(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Main");
+        Pane myPane = FXMLLoader.load(getClass().getResource("../xmlFiles/main.fxml"));
+        Scene scene = new Scene(myPane);
+        stage.setScene(scene);
+
+        prevStage.close();
+        stage.show();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import Controllers.MainController;
+import Controllers.WelcomeController;
 import Database.DbAdapter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/xmlFiles/welcome.fxml"));
         primaryStage.setTitle("JourneyPlanner");
-        primaryStage.setScene(new Scene(root));
+
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("xmlFiles/welcome.fxml"));
+
+        Pane myPane = myLoader.load();
+        WelcomeController controller = myLoader.getController();
+        controller.setPrevStage(primaryStage);
+
+        Scene myScene = new Scene(myPane);
+        primaryStage.setScene(myScene);
         primaryStage.show();
     }
 
