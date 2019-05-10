763eda455a243e80809bde78cc6b790bb89ea51b
mrzigod <mrzigod@gmail.com>
2019-05-10 17:06:50 +0200

    cities constraints

--- DataEngineering/JourneyPlannerDB.sql
+++ DataEngineering/JourneyPlannerDB.sql
@@ -23,7 +23,7 @@ CREATE TABLE cities (
     constraint rating_range check (rating>=0 and rating<=5),
     constraint minimum_price check (average_price>=0),
     constraint proper_name check (name similar to '[A-Z][a-z]*((-| )[A-Z][a-z]*)*'),
-    constraint proper_country check (name similar to '[A-Z][a-z]*((-| )[A-Z][a-z]*)*')
+    constraint proper_country check (country similar to '[A-Z][a-z]*((-| )[A-Z][a-z]*)*')
 );
 
 -- Table: bus_stops
