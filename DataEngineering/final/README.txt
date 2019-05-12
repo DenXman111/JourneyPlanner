46e76a971312d6debcbed6349984b7a000c8c0b4
Łukasz Selwa <selwa123@gmail.com>
2019-05-12 16:31:14 +0200

    files to submit for ID

---
+++ DataEngineering/final/README.txt
@@ -0,0 +1,30 @@
+Projekt „Journey Planner”  autorzy:
+Łukasz Selwa,
+Dzianis Pivavarau,
+Krzysztof Mrzigod
+
+Projekt bazy danych dla przewoźnika autobusowego.
+Baza danych umożliwia przechowywanie przejazdów autobusowych (tranists) odbywających się w ustalonych przedziałach czasowych (spans) regularnie co tydzień.
+Przejazdy te są między miastami europy, z których każde posiada indywidualną ocenę (rating) co pozwoli naszej aplikacji na znajdowanie podróży z najlepszą oceną.
+Dodatkowo baza umożliwia rezerwowanie miejsc w autobusach dla zarejestrowanych użytkowników.
+
+Bardziej szczegółowe elementy projektu i wraz z autorem:
+
+Łukasz Selwa:
+
+1. Widok ‚new_users’ nic nie przechowuje ale po wstawieniu wszystkich danych nowego użytkownika oblicza hash jego hasła i tworzy nową krotkę w tabeli users.
+
+2. Po zmodyfikowaniu lub usunięciu przedziału czasowego (span) wszystkie jego przerwy (breaks), które już nie będą w nim zawarte zostaną usunięte.
+
+3. Wyzwalacz check_span_overlap zapewnia, że wszystkie przedziały czasowe w ramach jednego przejazdu nigdy nie będą się nachodzić. (prawdopodobnie jego funkcjonalność zostanie zmieniona)
+
+4. Wyzwalacz break_check nie dodaje przerwy jeżeli nie jest zawarta w przedziale.
+
+5. Wyzwalacz seat_reservation_departure_date_check zapewnia, że nigdy nie będą dwie rezerwacje na to samo miejsce w autobusie.
+
+6. Wyzwalacz transit_reservation_check przed wstawieniem rezerwacji na przejazd sprawdza czy dany przejazd faktycznie odbędzie się w podanym terminie.
+
+7. Przykładowe dane dla tabel cities, bus_stops, new_users, buses_models, transits, spans, departure_time, reservations, transit_reservation, seat_reservation
+
+
+
---
+++ DataEngineering/final/clear.sql
@@ -0,0 +1,44 @@
+-- noinspection SqlResolveForFile
+
+-- script used for removing all data created after running file JourneyPlannerDB.sql
+DROP TABLE IF EXISTS breaks CASCADE;
+DROP TABLE IF EXISTS bus_stops CASCADE;
+DROP TABLE IF EXISTS buses_models CASCADE;
+DROP TABLE IF EXISTS cities CASCADE;
+DROP TABLE IF EXISTS departure_time CASCADE;
+DROP TABLE IF EXISTS spans CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS seat_reservation CASCADE;
+DROP TABLE IF EXISTS transit_reservation CASCADE;
+DROP TABLE IF EXISTS transits CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP VIEW IF EXISTS countries;
+DROP VIEW IF EXISTS lines;
+DROP VIEW IF EXISTS new_users;
+
+DROP SEQUENCE IF EXISTS reservation_id;
+DROP SEQUENCE IF EXISTS cities_id_seq;
+drop sequence if exists transit_id;
+drop sequence if exists span_id;
+DROP SEQUENCE IF EXISTS reservation_id_seq;
+DROP SEQUENCE IF EXISTS stops_id_seq;
+DROP SEQUENCE IF EXISTS transit_reservation_id_seq;
+
+DROP FUNCTION IF EXISTS break_check();
+DROP FUNCTION IF EXISTS date_reservation_check();
+DROP FUNCTION IF EXISTS have_free_seat_check();
+DROP FUNCTION IF EXISTS remove_breaks_after_delete();
+DROP FUNCTION IF EXISTS remove_breaks_after_update();
+DROP FUNCTION IF EXISTS seat_reservation_departure_date_check();
+DROP FUNCTION IF EXISTS seat_reservation_departure_date_check();
+DROP FUNCTION IF EXISTS check_span_overlap();
+drop function if exists add_bus(int,int,int,int,date,date,time,interval,day);
+DROP FUNCTION IF EXISTS get_buses(date, date);
+DROP FUNCTION IF EXISTS buses_in_span(numeric, date, date);
+DROP FUNCTION IF EXISTS add_bus(integer, integer, integer, integer, date, date, time, interval, integer);
+DROP FUNCTION IF EXISTS transit_reservation_check();
+
+DROP DOMAIN IF EXISTS email;
+DROP EXTENSION IF EXISTS citext;
+DROP EXTENSION IF EXISTS pgcrypto;
