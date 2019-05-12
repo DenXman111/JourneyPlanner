0b02ab0f028796f65b7128f9918943b276d614bc
mrzigod <mrzigod@gmail.com>
2019-05-12 16:36:12 +0200

    Accessing History

---
+++ src/main/java/HistoryController.java
@@ -0,0 +1,66 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class HistoryController implements Initializable {
+    private Stage prevStage;
+
+    @FXML
+    private Button ReturnButton;
+    @FXML
+    private VBox answersVBox;
+    @SuppressWarnings("WeakerAccess")
+    protected void setPrevStage(Stage stage){
+        this.prevStage = stage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<Trip> history = DbAdapter.getHistory(LoginController.username);
+        assert history != null;
+        answersVBox.getChildren().clear();
+        history.stream().
+                map(Trip::display).
+                forEach(node -> {
+                    answersVBox.getChildren().add(node);
+                    VBox.setMargin(node, new Insets(20, 10, 10, 20));
+                });
+    }
+
+    @FXML
+    void returnButtonPressed(ActionEvent event) throws IOException {
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/xmlFiles/welcome.fxml"));
+        Pane myPane = myLoader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("JourneyPlanner");
+
+        WelcomeController controller = myLoader.getController();
+        controller.setPrevStage(stage);
+        Scene myScene = new Scene(myPane);
+
+        stage.setScene(myScene);
+        stage.setResizable(false);
+        prevStage.close();
+        stage.show();
+
+    }
+}
